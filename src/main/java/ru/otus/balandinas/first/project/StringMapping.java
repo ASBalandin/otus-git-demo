aaef9345bdc9ada5b7a9421d0f2254a3295718b2
balandinas <balandinas@yandex.ru>
2023-11-15 19:51:48 +0300

    final version by BalandinAS 15/11/2023 19:51 curr info

--- src/main/java/ru/otus/balandinas/first/project/StringMapping.java
+++ src/main/java/ru/otus/balandinas/first/project/StringMapping.java
@@ -17,32 +17,32 @@ public class StringMapping {
         this.fraction = bd.remainder(BigDecimal.ONE).movePointRight(bd.scale()).abs().toBigInteger().intValue();
         this.whole = bd.setScale(0, RoundingMode.DOWN).abs().toBigInteger().longValueExact();
         this.currentCode = code;
-        description(whole,fraction,code);
+        description(whole, fraction, code);
         //System.out.println("целая часть = " + whole + "\r\nдробная часть = " + fraction);
     }
 
-    public String description(long whole, int fraction,  CurrencyCode code) {
+    public String description(long whole, int fraction, CurrencyCode code) {
         System.out.println("Число : " + whole + "." + String.format("%02d", fraction));
-        if (whole / 1000000000 >= 1000)
-        {
+        if (whole / 1000000000 >= 1000) {
             desc = "Извините, число к преобразованию превышает 999 миллиардов. Используйте, пожалуйста, меньшую разрядность.";
             return desc;
         }
         if (code == CurrencyCode.RUR) {
-            RusRub810 rub = new RusRub810(whole,fraction);
+            RusRub810 rub = new RusRub810(whole, fraction);
             rub.description(whole, fraction, rub, code);
             desc = rub.toString();
-        }
-        if (code == CurrencyCode.TRY) {
-            TurkishLira949 tl = new TurkishLira949(whole,fraction);
+        } else if (code == CurrencyCode.TRY) {
+            TurkishLira949 tl = new TurkishLira949(whole, fraction);
             tl.description(whole, fraction, tl, code);
             desc = tl.toString();
+        } else {
+            desc = "Извините, валюта недружественных стран не поддерживается";
         }
         return desc;
     }
 
     @Override
     public String toString() {
-        return  desc;
+        return desc;
     }
 }
